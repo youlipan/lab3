4c2201651ffcdc9b8235c435e26eafec26e708ff
Youli Pan <yopan@ucsd.edu>
2018-01-25 09:25:35 -0800

    Commit message

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
@@ -10,9 +11,50 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$('.jumbotron h1').text("I love Javascript");
+        
+        $(".jumbotron p").addClass("active");
+        $(".jumbotron p").toggleClass("active");
+        $("#testjs").text("Please wait...");
+
+    });
+       
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } 
+    else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	function projectClick(e) { 
+		console.log("Project clicked");
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    //$(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } 
+    else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(description).fadeToggle();
+       //$(".project-description").show();
+       //$(".project-description").toggle();
+       //$(".project-description").fadeIn();
+       //$(".project-description").fadeToggle();
+    }
 }
+
